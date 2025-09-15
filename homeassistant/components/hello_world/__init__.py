a4f7db9c5982802f4bca2eb78fac59f30adbc15f
BlueZoe <2944815131@qq.com>
2025-09-15 21:39:32 +0000

    feat:add hello_world integration

--- homeassistant/components/hello_world/__init__.py
+++ homeassistant/components/hello_world/__init__.py
@@ -2,34 +2,13 @@
 
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-_PLATFORMS: list[Platform] = [Platform.LIGHT]
-
-# TODO Create ConfigEntry type alias with API object
-# TODO Rename type alias and update all entry annotations
-type New_NameConfigEntry = ConfigEntry[MyApi]  # noqa: F821
+from .const import DOMAIN
 
 
-# TODO Update entry annotation
-async def async_setup_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
-    """Set up hello_world from a config entry."""
-
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # entry.runtime_data = MyAPI(...)
-
-    await hass.config_entries.async_forward_entry_setups(entry, _PLATFORMS)
-
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the hello_world integration."""
+    hass.states.async_set(f"{DOMAIN}.Hello_World", "Works!")
     return True
-
-
-# TODO Update entry annotation
-async def async_unload_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, _PLATFORMS)
